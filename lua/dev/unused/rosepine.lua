3d52dd3233fc93d7ee5b898914c44a31858a82b4
deveshahuja <deveshahuja9@gmail.com>
2025-07-01 02:58:01 +0530

    rosepine added

---
+++ lua/dev/unused/rosepine.lua
@@ -0,0 +1,17 @@
+-- lua/plugins/rose-pine.lua
+return {
+	"rose-pine/neovim",
+	name = "rose-pine",
+	config = function()
+        require("rose-pine").setup({
+            styles = {
+                italic = false,
+            },
+            -- highlight_groups = {
+            --     Comment = { italic = true }
+            -- }
+        })
+        vim.cmd("colorscheme rose-pine")
+
+    end
+}
